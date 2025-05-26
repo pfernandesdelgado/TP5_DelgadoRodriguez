2a3d59bebfb4e930a1dbe5917782a18724a86893
pfernandesdelgado <pfernandesdelgado@itba.edu.ar>
2025-05-26 18:42:12 -0300

    Delete 05_InfoMed_TP5_DelgadoRodriguez.sql

--- 05_InfoMed_TP5_DelgadoRodriguez.sql
+++
@@ -1,3 +0,0 @@
-UPDATE Pacientes SET ciudad = 'Buenos Aires' WHERE TRIM(LOWER(ciudad)) IN ('buenos aires', 'buenos   aires', 'bs aires', '  buenos aires', 'buenos aires ');
-UPDATE Pacientes SET ciudad = 'Córdoba' WHERE TRIM(LOWER(ciudad)) IN ('cordoba', 'córdoba', 'córodba');
-UPDATE Pacientes SET ciudad = 'Mendoza' WHERE TRIM(LOWER(ciudad)) IN ('mendoza', 'mendzoa');
