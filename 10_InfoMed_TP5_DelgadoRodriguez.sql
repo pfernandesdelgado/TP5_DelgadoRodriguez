844c67e98630760af1d8c4c9b474c32e725e2de6
pfernandesdelgado <pfernandesdelgado@itba.edu.ar>
2025-05-26 18:37:14 -0300

    Delete 10_InfoMed_TP5_DelgadoRodriguez.sql

--- 10_InfoMed_TP5_DelgadoRodriguez.sql
+++
@@ -1,3 +0,0 @@
-SELECT  c.id_consulta,  p.nombre AS nombre_paciente, c.fecha, c.diagnostico, c.tratamiento
-FROM  Consultas c
-JOIN Pacientes p ON c.id_paciente = p.id_paciente WHERE c.id_medico = 3 AND c.fecha BETWEEN '2024-08-01' AND '2024-08-31';
