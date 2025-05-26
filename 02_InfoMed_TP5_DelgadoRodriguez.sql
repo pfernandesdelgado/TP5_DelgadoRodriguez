1e6a2cd2a754da88bb3accbbb1ee25673b4c5053
pfernandesdelgado <pfernandesdelgado@itba.edu.ar>
2025-05-26 18:37:48 -0300

    Delete 02_InfoMed_TP5_DelgadoRodriguez.sql

--- 02_InfoMed_TP5_DelgadoRodriguez.sql
+++
@@ -1,4 +0,0 @@
-SELECT nombre, fecha_nacimiento,
-       (strftime('%Y', 'now') - strftime('%Y', fecha_nacimiento)) 
-       - (strftime('%m-%d', 'now') < strftime('%m-%d', fecha_nacimiento)) AS edad
-FROM Pacientes;
